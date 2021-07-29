c2662369cd52ccb7043a6d0d3744cff9b23bfad9
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-07-29 19:12:40 -0300

    Improve box functionality

--- contracts/Boxes.sol
+++ contracts/Boxes.sol
@@ -49,21 +49,6 @@ contract Boxes is ReentrancyGuard, ERC721, ERC721Holder
 		_setBaseURI("ipfs://");
 	}
 
-	function _chainId() internal pure returns (uint256 _chainid)
-	{
-		assembly { _chainid := chainid() }
-		return _chainid;
-	}
-
-	function mint(string memory _cid, address _to) external
-	{
-		uint256 _supply = totalSupply();
-		require(_supply < MINT_LIMIT, "limit exhausted");
-		uint256 _boxId = baseIndex + _supply;
-		_safeMint(_to, _boxId);
-		_setTokenURI(_boxId, _cid);
-	}
-
 	function boxOf(address _token, uint256 _tokenId) external view returns (uint256 _boxId, uint256 _i)
 	{
 		Index storage _index = indexes[_token][_tokenId];
@@ -81,46 +66,114 @@ contract Boxes is ReentrancyGuard, ERC721, ERC721Holder
 		return (_item.token, _item.tokenId);
 	}
 
+	function mint(string memory _cid, address _to) external
+	{
+		uint256 _supply = totalSupply();
+		require(_supply < MINT_LIMIT, "limit exhausted");
+		uint256 _boxId = baseIndex + _supply;
+		_safeMint(_to, _boxId);
+		_setTokenURI(_boxId, _cid);
+	}
+
+	function setTokenURI(uint256 _boxId, string memory _cid) external onlyOwner(_boxId)
+	{
+		_setTokenURI(_boxId, _cid);
+	}
+
 	function boxAddItem(uint256 _boxId, address _token, uint256 _tokenId) external onlyOwner(_boxId) nonReentrant
 	{
 		address _from = msg.sender;
-		require(_token != address(this), "not allowed");
-		IERC721(_token).transferFrom(_from, address(this), _tokenId);
-		uint256 _i = items[_boxId].length;
-		items[_boxId].push(Item({
-			token: _token,
-			tokenId: _tokenId
-		}));
-		indexes[_token][_tokenId] = Index({
-			boxId: _boxId,
-			i: _i
-		});
-		emit BoxAddItem(_boxId, _token, _tokenId);
+		_safeTransferFrom(_token, _from, _tokenId);
+		_boxAddItem(_boxId, _token, _tokenId);
 	}
 
 	function boxRemoveItem(uint256 _boxId, address _token, uint256 _tokenId, address _to) external onlyOwner(_boxId) nonReentrant
 	{
-		Index storage _index = indexes[_token][_tokenId];
-		require(_boxId == _index.boxId, "not found");
-		uint256 _i = _index.i;
-		_index.boxId = 0;
-		_index.i = 0;
-		uint256 _j = items[_boxId].length - 1;
-		if (_i < _j) {
-			items[_boxId][_i] = items[_boxId][_j];
+		_boxRemoveItem(_boxId, _token, _tokenId);
+		_safeTransfer(_token, _to, _tokenId);
+	}
+
+	function boxAddItemBatch(uint256 _boxId, address[] memory _tokens, uint256[] memory _tokenIds) external onlyOwner(_boxId) nonReentrant
+	{
+		address _from = msg.sender;
+		require(_tokens.length == _tokenIds.length, "length mismatch");
+		for (uint256 _i = 0; _i < _tokens.length; _i++) {
+			_safeTransferFrom(_tokens[_i], _from, _tokenIds[_i]);
 		}
-		items[_boxId].pop();
-		IERC721(_token).safeApprove(address(this), _tokenId);
-		IERC721(_token).transferFrom(address(this), _to, _tokenId);
-		emit BoxRemoveItem(_boxId, _token, _tokenId);
+		for (uint256 _i = 0; _i < _tokens.length; _i++) {
+			_boxAddItem(_boxId, _tokens[_i], _tokenIds[_i]);
+		}
+	}
+
+	function boxRemoveItemBatch(uint256 _boxId, address[] memory _tokens, uint256[] memory _tokenIds, address _to) external onlyOwner(_boxId) nonReentrant
+	{
+		require(_tokens.length == _tokenIds.length, "length mismatch");
+		for (uint256 _i = 0; _i < _tokens.length; _i++) {
+			_boxRemoveItem(_boxId, _tokens[_i], _tokenIds[_i]);
+		}
+		for (uint256 _i = 0; _i < _tokens.length; _i++) {
+			_safeTransfer(_tokens[_i], _to, _tokenIds[_i]);
+		}
 	}
 
 	function recoverItem(address _token, uint256 _tokenId, address _to) external nonReentrant
 	{
 		Index storage _index = indexes[_token][_tokenId];
-		require(_index.boxId == 0, "access denied");
+		require(_index.boxId == 0, "not found");
+		_safeTransfer(_token, _to, _tokenId);
+	}
+
+	function _boxAddItem(uint256 _boxId, address _token, uint256 _tokenId) internal
+	{
+		require(_token != address(this), "not allowed");
+		Index storage _index = indexes[_token][_tokenId];
+		require(_index.boxId == 0, "not found");
+		Item[] storage _items = items[_boxId];
+		uint256 _i = _items.length;
+		_items.push(Item({
+			token: _token,
+			tokenId: _tokenId
+		}));
+		_index.boxId = _boxId;
+		_index.i = _i;
+		emit BoxAddItem(_boxId, _token, _tokenId);
+	}
+
+	function _boxRemoveItem(uint256 _boxId, address _token, uint256 _tokenId) internal
+	{
+		Index storage _index = indexes[_token][_tokenId];
+		require(_index.boxId == _boxId, "not found");
+		uint256 _i = _index.i;
+		_index.boxId = 0;
+		_index.i = 0;
+		Item[] storage _items = items[_boxId];
+		uint256 _j = _items.length - 1;
+		if (_i < _j) {
+			Item storage _item = _items[_j];
+			indexes[_item.token][_item.tokenId].i = _i;
+			_items[_i] = _item;
+		}
+		_items.pop();
+		emit BoxRemoveItem(_boxId, _token, _tokenId);
+	}
+
+	function _safeTransfer(address _token, address _to, uint256 _tokenId) internal
+	{
+		require(_to != address(this), "not allowed");
 		IERC721(_token).safeApprove(address(this), _tokenId);
 		IERC721(_token).transferFrom(address(this), _to, _tokenId);
+	}
+
+	function _safeTransferFrom(address _token, address _from, uint256 _tokenId) internal
+	{
+		require(_from != address(this), "not allowed");
+		IERC721(_token).transferFrom(_from, address(this), _tokenId);
+	}
+
+	function _chainId() internal pure returns (uint256 _chainid)
+	{
+		assembly { _chainid := chainid() }
+		return _chainid;
 	}
 
 	event BoxAddItem(uint256 indexed _boxId, address indexed _token, uint256 indexed _tokenId);
